f25dae32c1fff04832d13333aaf0d5e4bc450d85
marekdano <mk.dano@gmail.com>
2015-11-30 10:40:07 +0000

    Added the logic of the game: find the winner and print the result to the terminal. Also added the images to the projects asset folder.

--- rock-paper-scissors.rb
+++ rock-paper-scissors.rb
@@ -9,15 +9,35 @@
 #
 # ###################################################################
 
-things = ["rock", "paper", "scissors"]
 
-puts things.include? "rock"
+things = {"rock" => 0, "paper" => 1, "scissors" => 2}
+random_num = Random.new
 
-# rock - scissors   0 - 2 = -2 wins
-# rock - paper      0 - 1 = -1 loses
-# paper - rock      1 - 0 = 1  wins
-# paper - scissors  1 - 2 = -1 loses
-# scissors - paper  2 - 1 = 1  wins
-# scissors - rock   2 - 0 = 2  loses
+def get_winner computer, user 
+  # rock - scissors   0 - 2 = -2 wins
+  # rock - paper      0 - 1 = -1 loses
+  # paper - rock      1 - 0 = 1  wins
+  # paper - scissors  1 - 2 = -1 loses
+  # scissors - paper  2 - 1 = 1  wins
+  # scissors - rock   2 - 0 = 2  loses
 
-puts "It's a tie!!!"
+  if [-2,1].include? (computer - user)
+    puts "You lost :("
+  elsif [-1,2].include? (computer - user)
+    puts "You win!!! Congratulation!!!"
+  elsif computer - user == 0
+    puts "It's a tie!!!"
+  else
+    puts "Something went wrong."
+  end
+end
+
+computer = random_num.rand(0..2)
+user = random_num.rand(0..2)
+
+puts "computer: #{things.key(computer)} 
+    user: #{things.key(user)}"
+
+
+get_winner computer, user
+
